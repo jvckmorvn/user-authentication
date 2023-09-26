2e1356e1cac344f8e36e14024de755acf481f2f8
Jack Moran <jvckmorvn@gmail.com>
2023-09-26 21:42:23 +0100

    remove hello world

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -19,7 +19,6 @@ function WelcomeScreen() {
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
-      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
